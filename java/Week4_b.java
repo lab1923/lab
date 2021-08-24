cf1f91d843f6c78e4915f1b0cd0f286ab1029df1
lab1923 <89344991+lab1923@users.noreply.github.com>
2021-08-24 12:29:19 +0530

    Update Week4_b.java

--- java/Week4_b.java
+++ java/Week4_b.java
@@ -12,15 +12,15 @@ int count = 0;
     Calculator(){
        count++;
     }
-    static int powerInt(double base ,double power){
+    static int powerInt(int base ,int power){
         int ans = 1;
         for(int i = 0;i < power ; i++)
             ans *= base;
         return ans;
     }
-    static double powerDouble(double base ,double power){
-        double ans = 1;
-        for(int i = 0;i < (int)power ; i++)
+    static double powerDouble(double base ,int power){
+        double ans = 1.0;
+        for(int i = 0;i < power ; i++)
             ans *= base;
         return ans;
     }
@@ -29,11 +29,12 @@ int count = 0;
 public class Week4_b{
     
     public static void main(String... args){
-        double a = Double.parseDouble(args[0]),b = Double.parseDouble(args[1]);
+        double a = Double.parseDouble(args[0]);
+        int b = Integer.parseInt(args[1]);
         Calculator obj = new Calculator();
         System.out.println(obj.powerInt(2,5));
         System.out.println(obj.powerInt(3,7));
-        System.out.println(obj.powerInt(a,b));
+        System.out.println(obj.powerDouble(a,b));
         System.out.println("The no of objects created are "+obj.count);
     }
 }
