dd1a509d4c2f676a6bb48bf9e13bcc6166c3e578
lab1923 <89344991+lab1923@users.noreply.github.com>
2021-08-25 22:09:21 +0530

    Update 4.1.py

--- Daa/4.1.py
+++ Daa/4.1.py
@@ -1,27 +1,27 @@
-# pivot at low
+#pivot as low
+
 import random
-def partition(arr,start,end):
-    pivot = arr[start]
-    low = start+1
-    high = end
-    pivot_index = low+1
-    while low < high:
-        while low <= high and arr[high] >= pivot:
-            high = high - 1
-        while low <= high and arr[low] <= pivot:
-            low = low + 1
-        if low <= high:
-            arr[low], arr[high] = arr[high], arr[low]
-    arr[start], arr[high] = arr[high], arr[start]
-    return high
+
+def partition(arr,low,high):
+    pivot = arr[low]
+    arr[low],arr[high] = arr[high],arr[low]
+    pivot_index = low
+    for i in range(low,high):
+        if pivot >= arr[i]:
+            arr[pivot_index],arr[i] = arr[i],arr[pivot_index]
+            pivot_index+=1
+    arr[pivot_index],arr[high] = arr[high],arr[pivot_index]
+    return pivot_index
 
 def quick_sort(arr,low,high):
+    
     if low < high :
         p = partition(arr,low,high)
         quick_sort(arr,low,p-1)
-        quick_sort(arr,p+1,high)
+        quick_sort(arr,p+1,high)        
+
 arr = []
-n = int(input("Enter the no of values to sort : "))
+n = int(input("Enter no of values :"))
 for _ in range(n):
     x = random.randint(0,100)
     arr.append(x)
