d5585494554540989a93173135a0d0348153da80
lab1923 <89344991+lab1923@users.noreply.github.com>
2021-08-25 21:40:04 +0530

    Update 4.1.py

--- Daa/4.1.py
+++ Daa/4.1.py
@@ -1,64 +1,30 @@
-#pivot as low
-import random as r
-import timeit
-import matplotlib.pyplot as plt
-import math
-com = 0
-def quick_sort(a,l,h):
-    if l < h:
-        p = partition(a,l,h)
-        quick_sort(a,l,p-1)
-        quick_sort(a,p+1,h)
-def partition(a,l,h):
-    pivot_index = l
-    pivot = a[pivot_index]
-    
-    
-    while l < h:
-        global com
-        while l < len(a) and a[l] <= pivot:
-            com += 1
-            l+=1
-        while a[h] > pivot:
-            com+=1
-            h-=1
-        
-        if l < h:
-            
-            a[l],a[h]=a[h],a[l]
-            com+=1
-    a[pivot_index],a[h]=a[h],a[pivot_index]
-    return h
-a = []
-comList = []
-print("N Time")
-for i in range(1,101):
-  a =[]
-  com =1
-  for j in range(0,i):
-    n = r.randint(1,100)
-    a.append(n)
-  start = timeit.default_timer()
-  quick_sort(a,0,len(a)-1)
-  end = timeit.default_timer()
-  if(i%10==0):
-      print(i,(end-start))
-  comList.append(com)
-  
-  
-#print(comList)
-n = [*range(1, 101, 1)]
-nn=[]
-for x in n:
-		nn.append(x*math.log(x,2))
-nn1 = []
-for x in n:
-    nn1.append(x*x)
-plt.plot(comList,n,color='green', linewidth=3,label='Quicksort time')
-plt.plot(nn,n,color='red', linewidth=3,label='nlogn')
-plt.plot(nn1,n,color='blue', linewidth=3,label='n^2')
-plt.title('Quick Sort Running ime')
-plt.xlabel('value of time')
-plt.ylabel('value of n')
-plt.legend()
-plt.show()
+# pivot at low
+import random
+def partition(arr,start,end):
+    pivot = arr[start]
+    low = start+1
+    high = end
+    pivot_index = low+1
+    while low < high:
+        while low <= high and arr[high] >= pivot:
+            high = high - 1
+        while low <= high and arr[low] <= pivot:
+            low = low + 1
+        if low <= high:
+            arr[low], arr[high] = arr[high], arr[low]
+    arr[start], arr[high] = arr[high], arr[start]
+    return high
+
+def quick_sort(arr,low,high):
+    if low < high :
+        p = partition(arr,low,high)
+        quick_sort(arr,low,p-1)
+        quick_sort(arr,p+1,high)
+arr = []
+n = int(input("Enter the no of values to sort : "))
+for _ in range(n):
+    x = random.randint(0,100)
+    arr.append(x)
+print(arr)
+quick_sort(arr,0,len(arr)-1)
+print(arr)
