1e098671fd9dcfca6a22131ff51ab7036615208e
lab1923 <89344991+lab1923@users.noreply.github.com>
2021-08-25 21:07:23 +0530

    Update 4.2.py

--- Daa/4.2.py
+++ Daa/4.2.py
@@ -1,66 +1,29 @@
-import random as r
-import timeit
-import matplotlib.pyplot as plt
-import math
-com = 0
-def quick_sort(a,l,h):
-    if l < h:
-        p = partition(a,l,h)
-        quick_sort(a,l,p-1)
-        quick_sort(a,p+1,h)
-def partition(a,l,h):
-    # pivot_index=h
-    # pivot = a[pivot_index]
-    pivot = max(a[l:h+1])
-    pivot_index = a.index(pivot)
-    a[l],a[pivot_index] = a[pivot_index],a[l]
-    pivot_index = l
+#pivot as high
+
+import random
+
+def partition(arr,low,high):
+    pivot = arr[high]
+    pivot_index = low
+    for i in range(low,high):
+        if pivot >= arr[i]:
+            arr[pivot_index],arr[i] = arr[i],arr[pivot_index]
+            pivot_index+=1
+    arr[pivot_index],arr[high] = arr[high],arr[pivot_index]
+    return pivot_index
+
+def quick_sort(arr,low,high):
     
-    while l < h:
-        global com
-        while l < len(a) and a[l] <= pivot:
-            com += 1
-            l+=1
-        while a[h] > pivot:
-            com+=1
-            h-=1
-        
-        if l < h:
-            
-            a[l],a[h]=a[h],a[l]
-            com+=1
-    a[pivot_index],a[h]=a[h],a[pivot_index]
-    return h
-a = []
-comList = []
-print("N Time")
-for i in range(1,101):
-  a =[]
-  com =1
-  for j in range(0,i):
-    n = r.randint(1,100)
-    a.append(n)
-  start = timeit.default_timer()
-  quick_sort(a,0,len(a)-1)
-  end = timeit.default_timer()
-  if(i%10==0):
-      print(i,(end-start))
-  comList.append(com)
-  
-  
-#print(comList)
-n = [*range(1, 101, 1)]
-nn=[]
-for x in n:
-		nn.append(x*math.log(x,2))
-nn1 = []
-for x in n:
-    nn1.append(x*x)
-plt.plot(comList,n,color='green', linewidth=3,label='Quicksort time')
-plt.plot(nn,n,color='red', linewidth=3,label='nlogn')
-plt.plot(nn1,n,color='blue', linewidth=3,label='n^2')
-plt.title('Quick Sort Running time')
-plt.xlabel('value of time')
-plt.ylabel('value of n')
-plt.legend()
-plt.show()
+    if low < high :
+        p = partition(arr,low,high)
+        quick_sort(arr,low,p-1)
+        quick_sort(arr,p+1,high)        
+
+arr = []
+n = int(input("Enter no of values :"))
+for _ in range(n):
+    x = random.randint(0,100)
+    arr.append(x)
+print(arr)
+quick_sort(arr,0,len(arr)-1)
+print(arr)
