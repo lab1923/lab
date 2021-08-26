edd62729201d6065bc459786b42d23c9bd13e28e
lab1923 <89344991+lab1923@users.noreply.github.com>
2021-08-26 08:20:42 +0530

    Update 10.py

--- Daa/10.py
+++ Daa/10.py
@@ -1,25 +1,25 @@
+method 2
 import numpy as np
-def floydWarshall(graph):
-    dist = list(map(lambda i: list(map(lambda j: j, i)), graph))
-    for k in range(V):
-        for i in range(V):
-            for j in range(V):
-                dist[i][j] = min(dist[i][j],dist[i][k] + dist[k][j])
-    printSolution(dist)
-def printSolution(dist):
-    print("The shortest distances are : ")
-    for i in range(V):
-        for j in range(V):
-            if(dist[i][j] == INF):
-                 print("%7s" % ("∞"),end =" ")
-            else:
-                print("%7d" % (dist[i][j]),end= " ")
-            if j == V-1:
-                print("")
+def floyd(graph ,v):
+    for k in range(v):
+        for i in range(v):
+            for j in range(v):
+                graph[i][j] = min(graph[i][j],graph[i][k]+graph[k][j])
+        print("After Step",k+1,"Matrix A: ")
+        printsol(graph,v)
+        
+def printsol(graph,v):
+    for i in range(v):
+        for j in range(v):
+            if(graph[i][j]==INF):
+                print(" %7s " %("INF"),end=" ")
+            else :
+                print(" %7d " %(graph[i][j]),end=" ")
+        print()
 V = 4
 INF = np.inf
-graph = np.array([[0, 3, INF, 7],
+graph = [[0, 3, INF, 7],
          [3, 0, 2, INF],
          [3, INF, 0, 1],
-         [2, INF, INF, 0]])
-floydWarshall(graph)
+         [2, INF, INF, 0]]
+floyd(graph,V)
