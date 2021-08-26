e8233d0c790b2550c38056a2b233f26b68105471
lab1923 <89344991+lab1923@users.noreply.github.com>
2021-08-26 10:53:07 +0530

    Update 10.py

--- Daa/10.py
+++ Daa/10.py
@@ -1,25 +1,19 @@
-# method 2
-import numpy as np
-def floyd(graph ,v):
-    for k in range(v):
-        for i in range(v):
-            for j in range(v):
-                graph[i][j] = min(graph[i][j],graph[i][k]+graph[k][j])
-        print("After Step",k+1,"Matrix A: ")
-        printsol(graph,v)
-        
-def printsol(graph,v):
-    for i in range(v):
-        for j in range(v):
-            if(graph[i][j]==INF):
-                print(" %7s " %("INF"),end=" ")
-            else :
-                print(" %7d " %(graph[i][j]),end=" ")
-        print()
-V = 4
-INF = np.inf
-graph = [[0, 3, INF, 7],
-         [3, 0, 2, INF],
-         [3, INF, 0, 1],
-         [2, INF, INF, 0]]
-floyd(graph,V)
+import sys
+def floyd(g,n):
+    
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                g[i][j] = min(g[i][j],g[i][k]+g[k][j])
+    
+    print(g)
+    
+inf = sys.maxsize
+g = [
+    [0,3,inf,7],
+    [3,0,2,inf],
+    [3,inf,0,1],
+    [2,inf,inf,0]
+]
+floyd(g,4)
+                
